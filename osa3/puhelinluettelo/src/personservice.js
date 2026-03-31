3f06a7a8ca44d2285854d2f29df4bd1ebe5befe3
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-03-31 11:42:17 +0300

    -

---
+++ osa3/puhelinluettelo/src/personservice.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const baseUrl = '/api/persons'
+
+const getAll = () => axios.get(baseUrl).then(r => r.data)
+
+const create = (person) => axios.post(baseUrl, person).then(r => r.data)
+
+const remove = (id) => axios.delete(`${baseUrl}/${id}`)
+
+const update = (id, person) => axios.put(`${baseUrl}/${id}`, person).then(r => r.data)
+
+export default { getAll, create, remove, update }
