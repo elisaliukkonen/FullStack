7bea926303b284bf909451022f4e4fa2222a7229
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-03-31 12:33:19 +0300

    Update API base URL in personservice.js

--- osa2/puhelinluettelo/src/personservice.js
+++ osa2/puhelinluettelo/src/personservice.js
@@ -1,13 +1,10 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => axios.get(baseUrl).then(r => r.data)
-
 const create = (person) => axios.post(baseUrl, person).then(r => r.data)
-
 const remove = (id) => axios.delete(`${baseUrl}/${id}`)
-
 const update = (id, person) => axios.put(`${baseUrl}/${id}`, person).then(r => r.data)
 
 export default { getAll, create, remove, update }
