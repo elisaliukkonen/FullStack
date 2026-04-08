a646a8832d6247608775b0e9d2d7723dcebab284
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-08 09:11:47 +0300

    testin muokkaus ja lisäys

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -15,3 +15,18 @@ describe('total likes', () => {
       title: 'Go To Statement Considered Harmful',
       author: 'Edsger W. Dijkstra',
       url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list is empty equals zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+})
