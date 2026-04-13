66d332cdf0ac1e85e0044ec3905de468f0c871c3
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-13 11:18:27 +0300

    Update app.js

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -14,5 +15,6 @@ mongoose.connect(mongoUrl)
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
