5c1b32829e5a1e11176586815ba72d140edfdd32
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-21 08:13:11 +0300

    päivitys

--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
-
 let token = null
 
 const setToken = (newToken) => {
@@ -20,4 +19,9 @@ const create = async (newObject) => {
   return response.data
 }
 
-export default { getAll, create, setToken }
+const update = async (id, newObject) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newObject)
+  return response.data
+}
+
+export default { getAll, create, setToken, update }
