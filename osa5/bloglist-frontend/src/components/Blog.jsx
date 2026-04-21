ce788409584579ba310271e9c998891ec14c79dd
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-21 08:04:21 +0300

    blogi päivitys

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visible && (
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}</div>
+          <div>{blog.user ? blog.user.name : ''}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
