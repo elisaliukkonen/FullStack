99b5ea0f842903970e63a58fbcd206de9dbf5bfa
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-21 08:14:03 +0300

    päivitys tehtävää varten

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, handleLike }) => {
   const [visible, setVisible] = useState(false)
 
   const blogStyle = {
@@ -22,7 +22,10 @@ const Blog = ({ blog }) => {
       {visible && (
         <div>
           <div>{blog.url}</div>
-          <div>likes {blog.likes}</div>
+          <div>
+            likes {blog.likes}
+            <button onClick={() => handleLike(blog)}>like</button>
+          </div>
           <div>{blog.user ? blog.user.name : ''}</div>
         </div>
       )}
