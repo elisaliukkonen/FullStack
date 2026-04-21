5e9a8cea69d5e913c29a0bb832ee5befe061cfd9
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-21 08:36:22 +0300

    päivitys vitosta vatten

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -18,19 +18,24 @@ blogsRouter.post('/', async (request, response) => {
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
-
   const user = await User.findById(decodedToken.id)
-
   const blog = new Blog({
     ...request.body,
     user: user._id
   })
-
   const saved = await blog.save()
   user.blogs = user.blogs.concat(saved._id)
   await user.save()
-
   response.status(201).json(saved)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    request.body,
+    { new: true }
+  ).populate('user', { username: 1, name: 1 })
+  response.json(updatedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
