b5d94f379469d921c9d4196b358a2b5eb95e47d5
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-21 08:45:12 +0300

    kartta

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -134,7 +134,7 @@ const App = () => {
         <BlogForm createBlog={handleCreateBlog} />
       </Togglable>
 
-      {blogs.map(blog =>
+      {[...blogs].sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} handleLike={handleLike} />
       )}
     </div>
