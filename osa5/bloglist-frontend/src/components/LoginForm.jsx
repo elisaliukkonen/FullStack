0b59a2bc06f3da999a85d6053490e9e8c71e11f6
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-21 12:22:25 +0300

    Create LoginForm.jsx

---
+++ osa5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,28 @@
+const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => {
+  return (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
