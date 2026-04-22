f2425a434b439feae07867e28da2b8431aacc325
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-22 11:18:52 +0300

    Update BlogView.jsx

--- osa5/bloglist-frontend/src/components/BlogView.jsx
+++ osa5/bloglist-frontend/src/components/BlogView.jsx
@@ -1,4 +1,8 @@
 import { useNavigate } from 'react-router-dom'
+import { Paper, Typography, Button, Box } from '@mui/material'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp'
+import DeleteIcon from '@mui/icons-material/Delete'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 
 const BlogView = ({ blog, handleLike, handleDelete, user }) => {
   const navigate = useNavigate()
@@ -13,19 +17,45 @@ const BlogView = ({ blog, handleLike, handleDelete, user }) => {
   }
 
   return (
-    <div>
-      <h2>{blog.title} by {blog.author}</h2>
-      <div><a href={blog.url}>{blog.url}</a></div>
-      <div>
-        likes {blog.likes}
-        {user && <button onClick={() => handleLike(blog)}>like</button>}
-      </div>
-      <div>added by {blog.user ? blog.user.name : ''}</div>
-      {showDelete && (
-        <button onClick={() => handleDeleteAndNavigate(blog)}>delete</button>
-      )}
-      <button onClick={() => navigate('/')}>back</button>
-    </div>
+    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+      <Paper elevation={3} sx={{ padding: 4, width: 600 }}>
+        <Typography variant="h4" sx={{ mb: 2 }}>{blog.title}</Typography>
+        <Typography variant="subtitle1" sx={{ mb: 1 }}>by {blog.author}</Typography>
+        <Typography sx={{ mb: 1 }}>
+          <a href={blog.url} target="_blank" rel="noreferrer">{blog.url}</a>
+        </Typography>
+        <Typography sx={{ mb: 2 }}>likes {blog.likes}</Typography>
+        <Typography sx={{ mb: 2 }}>added by {blog.user ? blog.user.name : ''}</Typography>
+        <Box sx={{ display: 'flex', gap: 1 }}>
+          {user && (
+            <Button
+              variant="contained"
+              startIcon={<ThumbUpIcon />}
+              onClick={() => handleLike(blog)}
+            >
+              like
+            </Button>
+          )}
+          {showDelete && (
+            <Button
+              variant="contained"
+              color="error"
+              startIcon={<DeleteIcon />}
+              onClick={() => handleDeleteAndNavigate(blog)}
+            >
+              delete
+            </Button>
+          )}
+          <Button
+            variant="outlined"
+            startIcon={<ArrowBackIcon />}
+            onClick={() => navigate('/')}
+          >
+            back
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
   )
 }
 
