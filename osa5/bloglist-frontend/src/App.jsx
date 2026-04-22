547c1aaceeb81ad9d26a3dc2e3f3634983166423
elisaliukkonen <elisa.liukkonen1@gmail.com>
2026-04-22 09:50:10 +0300

    päivitys

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect, useRef } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, useParams, useNavigate } from 'react-router-dom'
 import Blog from './components/Blog'
 import BlogList from './components/BlogList'
 import BlogForm from './components/BlogForm'
 import BlogView from './components/BlogView'
+import NewBlog from './components/NewBlog'
 import LoginForm from './components/LoginForm'
-import Togglable from './components/Togglable'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -27,6 +27,7 @@ const Navigation = ({ user, handleLogout }) => {
   return (
     <div style={{ background: '#f0f0f0', padding: 10, marginBottom: 10 }}>
       <Link style={padding} to="/">blogs</Link>
+      {user && <Link style={padding} to="/create">create new</Link>}
       {user
         ? <span>{user.name} logged in <button onClick={handleLogout}>logout</button></span>
         : <Link style={padding} to="/login">login</Link>
@@ -42,8 +43,6 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [notification, setNotification] = useState(null)
   const [notificationType, setNotificationType] = useState('success')
-
-  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -86,13 +85,13 @@ const App = () => {
     setUser(null)
   }
 
-  const handleCreateBlog = async ({ title, author, url }) => {
+  const handleCreateBlog = async ({ title, author, url }, navigate) => {
     try {
       await blogService.create({ title, author, url })
       const updatedBlogs = await blogService.getAll()
       setBlogs(updatedBlogs)
-      blogFormRef.current.toggleVisibility()
       notify(`a new blog ${title} by ${author} added`)
+      navigate('/')
     } catch {
       notify('error creating blog', 'error')
     }
@@ -104,63 +103,4 @@ const App = () => {
       author: blog.author,
       url: blog.url,
       likes: blog.likes + 1,
-      user: blog.user ? blog.user.id : null
-    }
-    try {
-      const returnedBlog = await blogService.update(blog.id, updatedBlog)
-      setBlogs(blogs.map(b => b.id === blog.id ? { ...returnedBlog, user: blog.user } : b))
-    } catch {
-      notify('error updating blog', 'error')
-    }
-  }
-
-  const handleDelete = async (blog) => {
-    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
-      try {
-        await blogService.remove(blog.id)
-        setBlogs(blogs.filter(b => b.id !== blog.id))
-        notify(`Blog ${blog.title} deleted`)
-      } catch {
-        notify('error deleting blog', 'error')
-      }
-    }
-  }
-
-  const BlogViewWrapper = () => {
-    const { id } = useParams()
-    const blog = blogs.find(b => b.id === id)
-    return <BlogView blog={blog} handleLike={handleLike} handleDelete={handleDelete} user={user} />
-  }
-
-  return (
-    <Router>
-      <Navigation user={user} handleLogout={handleLogout} />
-      <Notification message={notification} type={notificationType} />
-
-      <Routes>
-        <Route path="/login" element={
-          <LoginForm
-            handleLogin={handleLogin}
-            username={username}
-            setUsername={setUsername}
-            password={password}
-            setPassword={setPassword}
-          />
-        } />
-        <Route path="/blogs/:id" element={<BlogViewWrapper />} />
-        <Route path="/" element={
-          <div>
-            {user && (
-              <Togglable buttonLabel="create new blog" ref={blogFormRef}>
-                <BlogForm createBlog={handleCreateBlog} />
-              </Togglable>
-            )}
-            <BlogList blogs={blogs} handleLike={handleLike} handleDelete={handleDelete} user={user} />
-          </div>
-        } />
-      </Routes>
-    </Router>
-  )
-}
-
-export default App
+      user: blog.user ? b
